80bc26902900ef17edd84d46930ce58b65160e07
Haruki <aratashiharuki@gmail.com>
2024-07-08 00:53:16 +0300

    Add create metrics endpoint

--- backend/src/modules/metrics/metrics.controller.ts
+++ backend/src/modules/metrics/metrics.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { MetricsService } from './metrics.service';
 import { CreateMetricDto } from './dto/create-metric.dto';
 import { ApiGuard } from '../projects/guards/api.guard';
+import { CreateMetricsDto } from './dto/create-metrics.dto';
 
 @Controller('metrics')
 export class MetricsController {
@@ -11,5 +12,13 @@ export class MetricsController {
     @UseGuards(ApiGuard)
     async createMetric(@Body() createMetricDto: CreateMetricDto, @Request() request: any) {
         await this.metricsService.createMetric(request.projectId, createMetricDto)
+    }
+
+    @Post('/mcreate')
+    @UseGuards(ApiGuard)
+    async createMetrics(@Body() createMetricsDto: CreateMetricsDto, @Request() request: any) {
+        for (let dto of createMetricsDto.payload) {
+            await this.metricsService.createMetric(request.projectId, dto)
+        }
     }
 }
