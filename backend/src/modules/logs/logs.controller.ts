a0aa33400af3022b0e5f23b501db9ed574abd6f6
Haruki <aratashiharuki@gmail.com>
2024-07-08 00:17:27 +0300

    Removed AuthGuard

--- backend/src/modules/logs/logs.controller.ts
+++ backend/src/modules/logs/logs.controller.ts
@@ -1,15 +1,14 @@
 import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { CreateLogDto } from './dto/create-log.dto';
-import { AuthGuard } from '../auth/guards/auth.guard';
-import { ProjectGuard } from '../projects/guards/project.guard';
 import { LogsService } from './logs.service';
+import { ApiGuard } from '../projects/guards/api.guard';
 
 @Controller('logs')
 export class LogsController {
     constructor(private readonly logsService: LogsService) {}
 
     @Post('/create')
-    @UseGuards(AuthGuard, ProjectGuard)
+    @UseGuards(ApiGuard)
     async createLog(@Body() createLogDto: CreateLogDto, @Request() request: any) {
         await this.logsService.createLog(request.projectId, createLogDto)
     }
--- backend/src/modules/metrics/metrics.controller.ts
+++ backend/src/modules/metrics/metrics.controller.ts
@@ -1,7 +1,5 @@
 import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { MetricsService } from './metrics.service';
-import { AuthGuard } from '../auth/guards/auth.guard';
-import { ProjectGuard } from '../projects/guards/project.guard';
 import { CreateMetricDto } from './dto/create-metric.dto';
 import { ApiGuard } from '../projects/guards/api.guard';
 
@@ -10,7 +8,7 @@ export class MetricsController {
     constructor(private readonly metricsService: MetricsService) {}
 
     @Post('/create')
-    @UseGuards(AuthGuard, ApiGuard)
+    @UseGuards(ApiGuard)
     async createMetric(@Body() createMetricDto: CreateMetricDto, @Request() request: any) {
         await this.metricsService.createMetric(request.projectId, createMetricDto)
     }
