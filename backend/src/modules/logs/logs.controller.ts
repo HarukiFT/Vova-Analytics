0f6f3a3262f9c6e0ce5fae2b17766831d36af843
Haruki <aratashiharuki@gmail.com>
2024-07-08 00:51:27 +0300

    Add create logS method

--- backend/src/modules/logs/logs.controller.ts
+++ backend/src/modules/logs/logs.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { CreateLogDto } from './dto/create-log.dto';
 import { LogsService } from './logs.service';
 import { ApiGuard } from '../projects/guards/api.guard';
+import { CreateLogsDto } from './dto/create-logs.dto';
 
 @Controller('logs')
 export class LogsController {
@@ -9,7 +10,15 @@ export class LogsController {
 
     @Post('/create')
     @UseGuards(ApiGuard)
-    async createLog(@Body() createLogDto: CreateLogDto, @Request() request: any) {
+    async createLog(@Body() createLogDto: CreateLogDto, @Request() request: any): Promise<undefined> {
         await this.logsService.createLog(request.projectId, createLogDto)
+    }
+
+    @Post('/mcreate')
+    @UseGuards(ApiGuard)
+    async createLogs(@Body() createLogsDtoArray: CreateLogsDto, @Request() request: any): Promise<undefined> {
+        for (let dto of createLogsDtoArray.payload) {
+            await this.logsService.createLog(request.projectId, dto)
+        }
     }
 }
